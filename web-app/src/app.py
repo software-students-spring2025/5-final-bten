46951c622124a8cb0f12a000c9138feac53352a8
nikitabhaskar <nikitab2204@gmail.com>
2025-04-22 13:03:19 -0400

    added tests and pipfile

--- web-app/src/app.py
+++ web-app/src/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request, render_template
-import requests, os
+import requests
+import os
 from dotenv import load_dotenv
 
 
-
 def create_app():
-    app=Flask(__name__)
+    app = Flask(__name__)
     load_dotenv()
 
     """
@@ -14,20 +14,25 @@ def create_app():
     db = client.get_database("Recipe")
     collection = db.get_collection("logs")
     
-    """ 
+    """
 
     @app.route("/")
     def home():
         """Home Route"""
         return render_template("index.html")
 
-    @app.route("/predict", methods = ["POST"])
+    @app.route("/predict", methods=["POST"])
     def predict():
-        data = request.json
+        data = request.get_json()
         ingredient = data.get("ingredients")
-        #call chatgpt here
+        # call chatgpt here
+        recipe = {"recipe" : "recipe"}
+        return jsonify(recipe)
+        
+    
     return app
 
+
 if __name__ == "__main__":
-    app=create_app()
+    app = create_app()
     app.run(debug=True)
